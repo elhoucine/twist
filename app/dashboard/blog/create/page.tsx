eb2127c669dadaf509f97c3c01a41b7c4c695c38
Elhoucine <houcineparade@gmail.com>
2023-12-20 17:20:12 +0100

    add post layout main fields

--- app/dashboard/blog/create/page.tsx
+++ app/dashboard/blog/create/page.tsx
@@ -1,9 +1,168 @@
-import React from 'react'
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
 
-export default function page() {
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+} from "@/components/ui/form"
+import { toast } from "@/components/ui/use-toast"
+import { RocketIcon, StarIcon } from "lucide-react"
+import { Switch } from "@/components/ui/switch"
+import { BsSave } from 'react-icons/bs';
+import { useState } from "react"
+import { EyeOpenIcon, Pencil1Icon } from "@radix-ui/react-icons"
+import { Input } from "@/components/ui/input"
+import { cn } from "@/lib/utils"
+
+const FormSchema = z.object({
+  title: z.string().min(2, {
+    message: "Title must be at least 2 characters."
+  }),
+  image_url: z.string().url({ message: "Invalid link." }),
+  content: z.string().min(2, {
+    message: "Content must be at least 100 characters."
+  }),
+  is_published: z.boolean(),
+  is_premium: z.boolean(),
+})
+
+export default function BlogForm() {
+  const [isPreview, setIsPreview] = useState(false);
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+      image_url: "",
+      is_premium: false,
+      is_published: true,
+    },
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
+
   return (
-    <div>
-      Create
-    </div>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full border rounded-md space-y-6"
+      >
+        <div className="p-5 flex items-center flex-wrap justify-between border-b gap-5">
+          <div className="flex gap-5 items-center">
+            <span
+              role="button"
+              tabIndex={0}
+              className="flex items-center gap-1 border 
+            bg-zinc-700 p-2 rounded-md hover:ring-2 hover:ring-zinc-400 transition-all"
+              onClick={() => setIsPreview(!isPreview)}
+            >
+              {isPreview
+                ? (
+                  <>
+                    <EyeOpenIcon />
+                    Preview
+                  </>
+                )
+                : (
+                  <>
+                    <Pencil1Icon />
+                    Edit
+                  </>
+                )}
+            </span>
+
+            <FormField
+              control={form.control}
+              name="is_premium"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex items-center gap-1 border bg-zinc-700 p-2
+                   rounded-md">
+                      <StarIcon />
+                      <span>Premium</span>
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="is_published"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex items-center gap-1 border bg-zinc-700 p-2
+                   rounded-md">
+                      <RocketIcon />
+                      <span>Publish</span>
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </div>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+          </div>
+
+          <Button className="flex items-center gap-1">
+            <BsSave /> Save
+          </Button>
+
+        </div>
+
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <div className={cn("p-2 w-full flex break-words gap-2",
+                  isPreview ? "divide-x-0" : "divide-x",)}>
+                  <Input
+                    placeholder=""
+                    value={field.value}
+                    onChange={field.onChange}
+                    className={cn("border-none text-lg font-medium leading-relaxed",
+                      isPreview ? "w-0 p-0" : "w-full lg:w-1/2")}
+                  />
+                  <div className={cn(
+                    "lg:px-10",
+                    isPreview ? "mx-auto w-full lg:w-4/5" : "w-1/2 lg:block hidden")}>
+                    <h1 className="text-3xl font-medium">
+                      {form.getValues().title}
+                    </h1>
+                  </div>
+                </div>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+      </form>
+    </Form>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Navbar from '@/components/nav/Navbar'
 import { ThemeProvider } from '@/components/ui/theme-provider'
 import SessionProvider from '@/components/SessionProvider'
+import { Toaster } from '@/components/ui/toaster'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -30,6 +31,7 @@ export default function RootLayout({
             <Navbar/>
             {children}
           </main>
+          <Toaster/>
         </ThemeProvider>
         <SessionProvider/>
       </body>
