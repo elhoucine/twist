a94e5bcdab8a3159e37a79db4b6838b8446751f1
Elhoucine <houcineparade@gmail.com>
2023-12-23 18:51:39 +0100

    insert post to db

--- app/dashboard/blog/create/page.tsx
+++ app/dashboard/blog/create/page.tsx
@@ -3,18 +3,36 @@ import React from 'react'
 import { toast } from "@/components/ui/use-toast"
 import BlogForm from '../../components/BlogForm'
 import { BlogFormSchematype } from '../../schema';
+import { createBlog } from '@/lib/ations/blog';
 
 export default function page() {
 
-  const handleCreate = (data: BlogFormSchematype) => {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-full rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    })
+  const handleCreate = async (data: BlogFormSchematype) => {
+    const result = await createBlog(data);
+    const { error } = JSON.parse(result);
+
+    if(error?.message) {
+      toast({
+        title: "Failed to create post.",
+        description: (
+          <pre className="mt-2 w-full rounded-md bg-slate-950 p-4">
+            <code className="text-white">{error.message}</code>
+          </pre>
+        ),
+      })
+    } else {
+      toast({
+        title: "Successfully create " + data.title,
+      })
+    }
+    // toast({
+    //   title: "You submitted the following values:",
+    //   description: (
+    //     <pre className="mt-2 w-full rounded-md bg-slate-950 p-4">
+    //       <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+    //     </pre>
+    //   ),
+    // })
   }
   return (
    <BlogForm onSubmit={handleCreate} />
---
+++ lib/ations/blog.ts
@@ -0,0 +1,42 @@
+"use server";
+import { BlogFormSchematype } from '@/app/dashboard/schema';
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+import { Database } from '../types/supabase';
+const cookieStore = cookies()
+
+const supabase = createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+        cookies: {
+            get(name: string) {
+                return cookieStore.get(name)?.value
+            },
+            // set(name: string, value: string, options: CookieOptions) {
+            //   cookieStore.set({ name, value, ...options })
+            // },
+            // remove(name: string, options: CookieOptions) {
+            //   cookieStore.set({ name, value: '', ...options })
+            // },
+        },
+    }
+)
+
+export async function createBlog(data: BlogFormSchematype) {
+    const { ['content']: excludedKey, ...blog } = data;
+    const resultBlog = await supabase.from("blog")
+        .insert(blog)
+        .select('id')
+        .single();
+
+    if (resultBlog.error) {
+        // TODO: should remove inserted blog?
+        return JSON.stringify(resultBlog);
+    } else {
+        const result = await supabase.from('blog_content')
+            .insert({ blog_id: resultBlog.data.id, content: data.content })
+            // TODO: revalidation
+        return JSON.stringify(result);
+    }
+}
